a6f338401f8fdd889e14185537aa24ccf0172997
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-04-15 23:19:19 +0000

    fixed whitespace and dox

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,14 +4,13 @@
 *binary_tree_preorder- traverses through tree preordered
 *@tree: pointer to the tree to traverse
 *@func: pointer to func for each node
-Return: returns a void
 */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	
+
 	func(tree->n);
 
 	binary_tree_preorder(tree->left, func);
