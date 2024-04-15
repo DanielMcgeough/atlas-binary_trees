29af84980d8ea41255b4af7c2f9a09f28e8c52a9
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-04-15 23:46:24 +0000

    simplicity is elegant

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+*binary_tree_leaves- counts the leaves of a tree
+*@tree: pointer to root node of tree
+Return: returns number of leaves
+*/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
