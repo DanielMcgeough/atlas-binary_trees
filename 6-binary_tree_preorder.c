cbd365e675e4ded79125f27889654211da529272
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-04-15 23:17:00 +0000

    might run it in one

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+*binary_tree_preorder- traverses through tree preordered
+*@tree: pointer to the tree to traverse
+*@func: pointer to func for each node
+Return: returns a void
+*/
+
+void binary_tree_t(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
