e8a9e4fd9b08ee9c24762f1e3caffa2d55389315
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-04-16 00:01:24 +0000

    fixed tabs instead of spaces

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,13 +8,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height;
-    size_t right_height;
+	size_t left_height;
+	size_t right_height;
 
-    if (tree == NULL)
-        return (0);
-    
-    left_height = binary_tree_height(tree->left);
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
